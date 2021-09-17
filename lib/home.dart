0faafe32448b3b8b1c4b7130e456f2462c6501e8
rieull0225 <21500261@handong.edu>
2021-09-18 02:21:25 +0900

    image2

--- lib/home.dart
+++ lib/home.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'onBoarding.dart';
-import 'package:flutter/services.dart';
-import 'package:degree/search_result.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -19,10 +16,15 @@ class _HomeState extends State<Home> {
       appBar: null,
       body : Stack(
         children: [
-          Image.asset("assets/image/home/홈_배경_동그라미.png"),
+          SizedBox(width : 200, height : 200,
+              child: Image.asset('assets/image/AI진료/AI진료프로필.png')),
           Column(
             children: [
-              Text("hello")
+              Text("hello"),
+              Image.asset('assets/image/AI진료/AI진료프로필.png'),
+              Image.asset('assets/image/기타/온보딩1.png'),
+              Image.asset('assets/image/하단바/하단바_홈_활성.png')
+
             ],
           )
 
